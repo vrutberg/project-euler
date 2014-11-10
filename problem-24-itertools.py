7ccf97cb8bf68e2f5fddfddf9559f9116e646975
Viktor Rutberg <wishie@gmail.com>
2014-11-10 01:15:56 +0100

    Smarter output in itertools version of problem 24

--- problem-24-itertools.py
+++ problem-24-itertools.py
@@ -6,7 +6,12 @@ import itertools
 class Problem24():
 
     def solve(self, n):
-        return "".join("{0}".format(n) for n in list(itertools.permutations(range(n)))[999999])
+        index = 0
+        for n in itertools.permutations(range(n)):
+            if index == 999999:
+                return "".join("{0}".format(i) for i in n)
+                break
+            index += 1
 
 if __name__ == "__main__":
     problem = Problem24()
