68e050b4171fae1faed65cab45e71236441b9b21
Viktor Rutberg <wishie@gmail.com>
2014-11-27 00:01:53 +0100

    Problem 3

---
+++ problem-3.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import math
+
+from problem import Problem
+
+class Problem3(Problem):
+
+    def isPrime(self, n):
+        for i in xrange(2, n/2):
+            if n % i == 0:
+                return False
+
+        return True
+
+    def solve(self, n):
+        r = -1
+
+        for i in xrange(2, int(math.ceil(math.sqrt(n)))):
+            if (n / float(i)) % 1.0 == 0.0 and self.isPrime(i):
+                r = i
+
+        return r
+
+if __name__ == "__main__":
+    Problem3().solve_with_timing(600851475143)
