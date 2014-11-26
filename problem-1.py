f4dddb381f57efac1a760c5d6c4dc3c916749197
Viktor Rutberg <wishie@gmail.com>
2014-11-26 22:49:42 +0100

    Let's use the n parameter

--- problem-1.py
+++ problem-1.py
@@ -7,7 +7,7 @@ class Problem1(Problem):
     def solve(self, n):
         sum = 0
 
-        for i in range(999):
+        for i in range(n-1):
             num = i+1
 
             if num % 3 == 0 or num % 5 == 0:
