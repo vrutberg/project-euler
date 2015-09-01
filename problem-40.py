e24bfddbbde2deaa0f4d5852afb3226bc1537514
Viktor Rutberg <wishie@gmail.com>
2015-09-01 22:50:30 +0200

    Added solution to problem 40.

---
+++ problem-40.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import math
+
+from problem import Problem
+
+class Problem40(Problem):
+
+    def get_index(self, n):
+        return int("".join([str(x) for x in range(1, n+1)])[n-1])
+
+    def solve(self, numbers):
+        product = 1
+
+        for n in numbers:
+            product *= self.get_index(n)
+
+        return product
+
+if __name__ == "__main__":
+    Problem40().cli_interface([1, 10, 100, 1000, 10000, 100000, 1000000])
