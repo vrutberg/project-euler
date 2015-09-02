0f3e839304b015585a25ccd73353839c2ab327d2
Viktor Rutberg <wishie@gmail.com>
2015-09-02 21:27:10 +0200

    Problem 4

---
+++ problem-4.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from problem import Problem
+
+class Problem4(Problem):
+
+    def solve(self, n):
+        biggest = 0
+        r = range(2, pow(10, n)) 
+
+        for i in r: 
+            for j in r:
+                product = i * j
+
+                if str(product) == str(product)[::-1] and product > biggest:
+                    biggest = product
+
+        return biggest
+
+if __name__ == "__main__":
+    Problem4().cli_interface(3)
